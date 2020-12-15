4676428e6671c2460a69f71e24021741b2e6e7cd
Austin Riendeau <austin@apriendeau.com>
2020-12-15 16:47:33 -0700

    work

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./components/home";
+import About from "./components/about";
+import "./App.scss";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <header className="header row">
+          <div className="column column-50 column-offset-33 header-text">
+            <h1>Austin Riendeau</h1>
+          </div>
+        </header>
+
+        <nav className="sidebar-nav row">
+          <div className="mobile-nav column column-33 column-offset-33">
+            <ul>
+              <li>
+                <Link to="/">home</Link>
+              </li>
+              <li>
+                <Link to="/about">about</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <main className="row">
+          <Switch>
+            <Route path="/about" component={About} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </main>
+        <footer className="row"></footer>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/components/about.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="column">
+          <h2>About Me</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
