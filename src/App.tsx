e73a28c1be6c6994200872ff67651fbafe7db8e8
Austin Riendeau <austin@apriendeau.com>
2020-12-16 20:10:50 -0700

    simple

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,37 @@ import React from "react";
 import Home from "./components/home";
 import About from "./components/about";
 import "./App.scss";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+//import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <Router>
-      <div className="container">
-        <header className="header row">
-          <div className="column column-50 column-offset-33 header-text">
+      <div className="pure-g">
+        {/* <header className="header pure-u-1">
             <h1>Austin Riendeau</h1>
-          </div>
-        </header>
+            </header> */}
 
-        <nav className="sidebar-nav row">
-          <div className="mobile-nav column column-33 column-offset-33">
+        {/* <nav className="sidebar-nav pure-u-1">
+            <div>
             <ul>
-              <li>
-                <Link to="/">home</Link>
-              </li>
-              <li>
-                <Link to="/about">about</Link>
-              </li>
+            <li>
+            <Link to="/">home</Link>
+            </li>
+            <li>
+            <Link to="/about">about</Link>
+            </li>
             </ul>
-          </div>
-        </nav>
-
-        <main className="row">
+            </div>
+            </nav>
+	  */}
+        <main className="pure-u-1">
           <Switch>
             <Route path="/about" component={About} />
             <Route path="/" component={Home} />
           </Switch>
         </main>
-        <footer className="row"></footer>
+        {/* <footer className="pure-u-1"></footer>*/}
       </div>
     </Router>
   );
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,11 +2,10 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="column">
-          <h2>Home</h2>
-        </div>
+    <div className="home pure-g">
+      <div className="pure-u-1 pure-u-md-1-3">&nbsp;</div>
+      <div className="center-text pure-u-1 pure-u-md-1-3">
+        <h1>I write code...</h1>
       </div>
     </div>
   );
